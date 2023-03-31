b807423264ac081ecb792a63c315b70bb39a85f3
suhasinipatil <suhasinibpatil09@gmail.com>
2023-03-31 16:41:08 +0530

    completed basic functionality

--- src/components/TodosLogic.js
+++ src/components/TodosLogic.js
@@ -1,22 +1,23 @@
 import InputTodo from "./InputTodo";
 import TodosList from "./TodosList";
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const TodosLogic = () => {
    // const [todos, setTodos] = useState([]);
     const [todos, setTodos] = useState([
         {
-            id: 1,
+            id: uuidv4(),
             title: 'Setup development environmet',
             completed: true,
         },
         {
-            id: 2,
+            id: uuidv4(),
             title: 'Develop website and add content',
             completed: false,
           },
           {
-            id: 3,
+            id: uuidv4(),
             title: 'Deploy to live server',
             completed: false,
           },
@@ -46,7 +47,7 @@ const TodosLogic = () => {
 
     const addTodoItem = (title) => {
         const newTodo = {
-            id : 4,
+            id : uuidv4(),
             title: title,
             completed: false,
         };
