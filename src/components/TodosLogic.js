07e28bb042ddf31f540e7548e6b7725b4d81260d
suhasinipatil <suhasinibpatil09@gmail.com>
2023-04-01 20:41:58 +0530

    added storage functionality

--- src/components/TodosLogic.js
+++ src/components/TodosLogic.js
@@ -1,27 +1,10 @@
 import InputTodo from "./InputTodo";
 import TodosList from "./TodosList";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 const TodosLogic = () => {
-   // const [todos, setTodos] = useState([]);
-    const [todos, setTodos] = useState([
-        {
-            id: uuidv4(),
-            title: 'Setup development environmet',
-            completed: true,
-        },
-        {
-            id: uuidv4(),
-            title: 'Develop website and add content',
-            completed: false,
-          },
-          {
-            id: uuidv4(),
-            title: 'Deploy to live server',
-            completed: false,
-          },
-    ]);
+    const [todos, setTodos] = useState(getInitialTodos());
 
     const handleChange = (id) => {
         setTodos((prevState) => 
@@ -64,6 +47,17 @@ const TodosLogic = () => {
             })
         );
     };
+
+    function getInitialTodos() {
+        const temp = localStorage.getItem('todos');
+        const savedTodos = JSON.parse(temp);
+        return savedTodos || [];
+    }
+
+    useEffect(() => {
+        const temp = JSON.stringify(todos);
+        localStorage.setItem('todos', temp);
+    } , [todos]);
 
     return(
         <div>
