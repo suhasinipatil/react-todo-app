cd6ba0f66f088d7b94ae16a21e1befec4c06920a
suhasinipatil <suhasinibpatil09@gmail.com>
2023-04-01 14:40:38 +0530

    added edit functionality

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,12 +1,12 @@
 import TodoItem from "./TodoItem";
 
-const TodosList = ({todosProps, handleChange, delTodo}) => {
+const TodosList = ({todosProps, handleChange, delTodo, setUpdate}) => {
     //const {todosProps} = props;
 
     return(
         <ul>
             {todosProps.map((todo) => (
-                <TodoItem key={todo.id} itemProp={todo} handleChange={handleChange} delTodo={delTodo}/>
+                <TodoItem key={todo.id} itemProp={todo} handleChange={handleChange} delTodo={delTodo} setUpdate={setUpdate}/>
             ))}
         </ul>
     );
